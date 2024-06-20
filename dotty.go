9881b3bf35b431ed74dfa2bbe71d663da181855e
utox39 <francescomoccaldi39@gmail.com>
2024-06-20 18:43:19 +0200

    Add LogErr implementation and improves error messages

--- dotty.go
+++ dotty.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"log"
 	"os"
 	"path/filepath"
 	"strings"
@@ -19,39 +18,38 @@ type Dotfiles struct {
 	DestinationPath string   `json:"destination-path"`
 }
 
-func LogError(err error) {
-	log.Fatalf("dotty: error: %v\n", err)
-}
-
 func Backup() error {
 	fmt.Println("Hello world from dotty :)")
 
 	// Get home folder
 	homeFolder, err := os.UserHomeDir()
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("error getting user home folder %v", err)
 	}
 
 	// Read json config file
 	jsonConfigPath := filepath.Join(homeFolder, ".config/dotty/config.json")
 	jsonFile, err := os.Open(jsonConfigPath)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("error opening config file %v", err)
 	}
 	defer func(jsonFile *os.File) {
 		err := jsonFile.Close()
 		if err != nil {
-			log.Fatalf("dotty: error closing the file %v: %v", jsonFile, err)
+			LogErr(fmt.Errorf("error closing the file %v: %v", jsonFile, err))
 		}
 	}(jsonFile)
 
-	byteValue, _ := io.ReadAll(jsonFile)
+	byteValue, err := io.ReadAll(jsonFile)
+	if err != nil {
+		return fmt.Errorf("error reading config file %v", err)
+	}
 
 	// Parse the json file
 	var dotfile Dotfiles
 	err = json.Unmarshal(byteValue, &dotfile)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("error unmarshalling json: %v", err)
 	}
 
 	destinationPath := dotfile.DestinationPath
@@ -60,8 +58,7 @@ func Backup() error {
 
 	// Check if the destination path exists
 	if _, err := os.Stat(destinationPath); errors.Is(err, os.ErrNotExist) {
-		fmt.Printf("dotty: %v: the directory does not exist.\n", destinationPath)
-		os.Exit(3)
+		return fmt.Errorf("%v: the directory does not exist.\n", destinationPath)
 	}
 
 	for i := 0; i < len(dotfile.Filepath); i++ {
@@ -76,92 +73,102 @@ func Backup() error {
 
 		if _, err := os.Stat(filePath); err == nil {
 			fmt.Printf("- Copying %v...\n", filePath)
-			CopyFile(filePath, destinationPath)
+			if err := CopyFile(filePath, destinationPath); err != nil {
+				return err
+			}
 		} else if errors.Is(err, os.ErrNotExist) {
 			fmt.Printf("dotty: %v does not exist", filePath)
 			continue
 		} else {
-			LogError(err)
+			return err
 		}
 	}
 
 	return nil
 }
 
-func CopyFile(filePath string, destinationPath string) {
+func CopyFile(filePath string, destinationPath string) error {
 	// Open the file and save the content
 	file, err := os.Open(filePath)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("could not open file %v: %v", filePath, err)
 	}
 	defer func(file *os.File) {
 		err := file.Close()
 		if err != nil {
-			log.Fatalf("dotty: error closing the file %v: %v", filePath, err)
+			LogErr(fmt.Errorf("error closing the file %v: %v", filePath, err))
 		}
 	}(file)
 
 	// Create a copy of the file to copy
 	newFile, err := os.Create(filepath.Join(destinationPath, filepath.Base(filePath)))
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("could not create file %v: %v", newFile, err)
 	}
 	defer func(newFile *os.File) {
 		err := newFile.Close()
 		if err != nil {
-			log.Fatalf("dotty: error closing the file %v: %v", filepath.Base(filePath), err)
+			LogErr(fmt.Errorf("error closing the file %v: %v", filepath.Base(filePath), err))
 		}
 	}(newFile)
 
 	// Write the content of the file to copy in the copy file
 	bytesWritten, err := io.Copy(newFile, file)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("could not copy file %v to %v: %v", file, newFile, err)
 	}
 	fmt.Printf("Copied %d bytes.\n", bytesWritten)
 
 	err = newFile.Sync()
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("error syncing file: %v", err)
 	}
 
 	fmt.Printf("%v copied successfully\n\n", filePath)
+
+	return nil
 }
 
 func AddFile(newFilePath string) error {
 	if _, err := os.Stat(newFilePath); errors.Is(err, os.ErrNotExist) {
-		fmt.Printf("dotty: %v does not exist\n", newFilePath)
+		return fmt.Errorf("%v does not exist\n", newFilePath)
 	} else if err != nil {
-		LogError(err)
+		return fmt.Errorf("add file error: %v", err)
 	}
 
 	// Get home folder
 	homeFolder, err := os.UserHomeDir()
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("error getting user home folder %v", err)
 	}
 
 	// Read json config file
 	jsonConfigPath := filepath.Join(homeFolder, ".config/dotty/config.json")
 	jsonFile, err := os.Open(jsonConfigPath)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("error opening the file %v: %v", jsonConfigPath, err)
 	}
 	defer func(jsonFile *os.File) {
 		err := jsonFile.Close()
 		if err != nil {
-			log.Fatalf("dotty: error closing the file %v: %v", jsonFile, err)
+			LogErr(fmt.Errorf("error closing the file %v: %v", jsonFile, err))
 		}
 	}(jsonFile)
 
-	byteValue, _ := io.ReadAll(jsonFile)
+	byteValue, err := io.ReadAll(jsonFile)
+	if err != nil {
+		return fmt.Errorf("error reading the file %v: %v", jsonConfigPath, err)
+	}
 
 	// Add the new dotfile to the end of the dotfiles array
-	value, _ := sjson.Set(string(byteValue), "dotfiles.-1", newFilePath)
+	value, err := sjson.Set(string(byteValue), "dotfiles.-1", newFilePath)
+	if err != nil {
+		return fmt.Errorf("error adding the new dotfile to the dotfiles array %v: %v", jsonConfigPath, err)
+	}
 
 	err = os.WriteFile(jsonConfigPath, []byte(value), 0644)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("error writing the file %v: %v", jsonConfigPath, err)
 	}
 
 	return nil
@@ -178,7 +185,7 @@ func main() {
 				Action: func(cCtx *cli.Context) error {
 					err := AddFile(cCtx.Args().Get(0))
 					if err != nil {
-						LogError(err)
+						LogErr(err)
 					}
 					return nil
 				},
@@ -187,7 +194,7 @@ func main() {
 		Action: func(*cli.Context) error {
 			err := Backup()
 			if err != nil {
-				LogError(err)
+				LogErr(err)
 			}
 			return nil
 		},
@@ -195,6 +202,6 @@ func main() {
 	}
 
 	if err := app.Run(os.Args); err != nil {
-		log.Fatal(err)
+		LogErr(err)
 	}
 }
